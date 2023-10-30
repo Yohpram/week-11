0c3f5921a4b13273364bca8ef9c676cd02775245
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2023-10-30 12:29:57 +0700

    Add files via upload

---
+++ test/todo.test.js
@@ -0,0 +1,23 @@
+const app = require('../app')
+const request = require ('supertest')
+
+describe('test api todo', () => {
+    
+    describe('get todo', () => {
+        test ('get 200 all todo', (done) => {
+            request(app)
+            .get('/todo')
+            .expect('Content-type', /json/)
+            .expect(200)
+            .then (response => {
+                expect(response.body.massage).toBe('success')
+                expect(response.body.data.length).toBe(2)
+                done()
+            })
+            .catch (done)
+        });
+        
+        //test('GET 500 All todo', (done) =>{
+
+        });
+    });
