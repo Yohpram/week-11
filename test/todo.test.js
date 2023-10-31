0932693fdeac871b6b81e5ce1b3ab25065d435ef
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2023-10-31 19:01:07 +0700

    Update todo.test.js

--- test/todo.test.js
+++ test/todo.test.js
@@ -11,13 +11,52 @@ describe('test api todo', () => {
             .expect(200)
             .then((response) => {
                 expect(response.body.message).toBe('success');
-                expect(response.body.data.length).toBe(2);
+                expect(response.body.data.length).toBe(15);
                 done();
             })
             .catch(done);
         });
-        
-        //test('GET 500 All todo', (done) =>{
+
+        describe('POST /todo', () => {
+            test('should create a new todo', (done) => {
+                const newTodoData = {
+                    nama: 'Pram',
+                    hari: 'Monday',
+                    kegiatan: 'Do something',
+                };
+    
+                request(app)
+                    .post('/todo')
+                    .send(newTodoData)
+                    .expect('Content-type', /json/)
+                    .expect(201) // Assuming you return HTTP status 201 for a successful creation
+                    .then((response) => {
+                        expect(response.body.nama).toBe(newTodoData.nama);
+                        expect(response.body.hari).toBe(newTodoData.hari);
+                        expect(response.body.kegiatan).toBe(newTodoData.kegiatan);
+                        done();
+                    })
+                    .catch(done);                   
+            });
+
+            describe('delete /todo/:id', () => {
+        test('should delete a todo', (done) => {
+            
+            const todoIdToDelete = 16; 
+
+            request(app)
+                .delete(`/todo/${todoIdToDelete}`)
+                .expect('Content-type', /json/)
+                .expect(201) 
+                .then((response) => {
+                    expect(response.body.message).toBe('movie deleted succesfully');
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
 
         });
     });
+});
